21474fd9426538319da8cf3b30a73acddd00f185
vaishnavitayade08 <100427856+vaishnavitayade08@users.noreply.github.com>
2022-02-25 21:16:32 +0530

    Array Practice

---
+++ Solution.java
@@ -0,0 +1,16 @@
+package JavaBasicProgramming;
+
+
+public class Solution{
+
+		 public static void main(String[]args) {
+			 int[] ab= {1,2,3,4,5};
+			 
+			 for (int i=0;i<ab.length;i++) {
+				 int a=ab[i];
+				 if(a%2==0)
+			 System.out.println(ab[i]);
+			  
+		 }
+	}
+}
